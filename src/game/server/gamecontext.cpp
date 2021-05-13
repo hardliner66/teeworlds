ac2adab07e4019a02bc675300cc729717a400d41
hardliner66 <hardliner66@gmail.com>
2021-05-13 21:24:17 +0200

    allow emote spam with spam protection

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1225,7 +1225,7 @@ void CGameContext::OnMessage(int MsgID, CUnpacker *pUnpacker, int ClientID)
 		{
 			CNetMsg_Cl_Emoticon *pMsg = (CNetMsg_Cl_Emoticon *)pRawMsg;
 
-			if(g_Config.m_SvSpamprotection && pPlayer->m_LastEmote && pPlayer->m_LastEmote+Server()->TickSpeed()*3 > Server()->Tick())
+			if(!g_Config.m_SvAllowEmoteSpam && g_Config.m_SvSpamprotection && pPlayer->m_LastEmote && pPlayer->m_LastEmote+Server()->TickSpeed()*3 > Server()->Tick())
 				return;
 
 			pPlayer->m_LastEmote = Server()->Tick();
